a14e880e01ef47f83070c0ecdf00f062257691b6
Relica Malinao <relicamalinao@gmail.com>
2026-06-08 22:27:00 +0800

    Update 5_Achievements.py

--- pages/5_Achievements.py
+++ pages/5_Achievements.py
@@ -7,7 +7,7 @@ st.set_page_config(
 )
 
 # LOAD CSS
-with open("styles/style.css") as f:
+with open("styles/styles.css") as f:
     st.markdown(
         f"<style>{f.read()}</style>",
         unsafe_allow_html=True
